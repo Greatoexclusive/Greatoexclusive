67efecb48b42e8a7d9e3574f37fa013b3c233fb2
greatoexclusive <greatoexclusive@gmail.com>
2022-02-05 15:43:10 +0100

    firstcommit

---
+++ lib/src/app.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
+import 'dart:core';
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    final response =
+        await get(Uri.parse('http://jsonplaceholder.typicode.com/photos'));
+    final imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ImageList(images),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: fetchImage,
+        ),
+        appBar: AppBar(
+          title: Text('GSUBZ'),
+        ),
+      ),
+    );
+  }
+}
